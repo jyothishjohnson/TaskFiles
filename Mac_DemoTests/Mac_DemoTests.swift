38a9a3417861013eb01575b9b84b6ccb0e0bb35f
Jyothish Johnson <jyothish.johnson@gmail.com>
2024-10-10 13:11:38 +0530

    Initial Commit

---
+++ Mac_DemoTests/Mac_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_DemoTests.swift
+//  Mac_DemoTests
+//
+//  Created by jyothish.johnson on 10/10/24.
+//
+
+import Testing
+@testable import Mac_Demo
+
+struct Mac_DemoTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
